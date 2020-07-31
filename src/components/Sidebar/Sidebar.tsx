a9f05d658ffed6bc7afacf58ec31689c3ca5730f
Martynas Druteika <Martynas.Druteika@geniussports.com>
2020-07-31 15:34:14 +0300

    Login form validation

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,32 +3,96 @@ import './Sidebar.scss';
 import {useState} from 'react';
 import {Button} from '../common/Button/Button';
 import {Checkbox} from '../common/Checkbox/Checkbox';
+import {joinTruthy} from "../../utils/utils";
 
 interface IFormInfo {
-    email: string;
-    password: string;
-    rememberMe: boolean
+    email: {
+        email: string,
+        error: string
+    };
+    password: {
+        password: string,
+        error: string
+    };
+    rememberMe: boolean;
 }
 
 export const Sidebar: React.FunctionComponent = () => {
     const [formInformation, setFormInformation] = useState<IFormInfo>({
-        email: '',
-        password: '',
+        email: {
+            email: '',
+            error: ''
+        },
+        password: {
+            password: '',
+            error: ''
+        },
         rememberMe: false
     });
 
-    const handleFormSubmit = () => console.log(formInformation);
+    const checkIsEmailValid = () => {
+        const emailPattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        const formInfoCopy = Object.assign({}, formInformation, {});
+        let isValid = true;
+        if (!formInformation.email.email) {
+            isValid = false;
+            formInfoCopy.email.error = 'Please enter your email.';
+        } else {
+            if (!emailPattern.test(formInfoCopy.email.email)) {
+                isValid = false;
+                formInfoCopy.email.error = 'Please enter valid email address.';
+            } else {
+                formInfoCopy.email.error = '';
+            }
+        }
+        setFormInformation(formInfoCopy);
+        return isValid;
+    }
+
+    const checkIsPasswordValid = () => {
+        const formInfoCopy = Object.assign({}, formInformation, {});
+        let isValid = true;
+        if (!formInfoCopy.password.password) {
+            isValid = false;
+            formInfoCopy.password.error = 'Please enter your password.';
+        } else {
+            if (formInfoCopy.password.password.length < 6) {
+                isValid = false;
+                formInfoCopy.password.error = 'Your password must contain at least 6 symbols.';
+            } else {
+                formInfoCopy.password.error = '';
+            }
+        }
+        setFormInformation(formInfoCopy);
+        return isValid;
+    }
+
+    const validateForm = () => {
+        const isEmailValid = checkIsEmailValid();
+        const isPasswordValid = checkIsPasswordValid();
+
+        return isEmailValid && isPasswordValid;
+    }
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (validateForm()) {
+            alert('Form submitted!')
+        }
+    };
 
     const handleEmailInputOnBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
         const formInfoCopy = Object.assign({}, formInformation, {});
-        formInfoCopy.email = e.target.value;
+        formInfoCopy.email.email = e.target.value;
         setFormInformation(formInfoCopy);
+        checkIsEmailValid();
     }
 
     const handlePasswordInputOnBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
         const formInfoCopy = Object.assign({}, formInformation, {});
-        formInfoCopy.password = e.target.value;
+        formInfoCopy.password.password = e.target.value;
         setFormInformation(formInfoCopy);
+        checkIsPasswordValid();
     }
 
     const handleRememberMeInputOnChange = () => {
@@ -43,39 +107,59 @@ export const Sidebar: React.FunctionComponent = () => {
                 className='login-box'
             >
                 <h3 className='login-box__title'>Client zone</h3>
-                <form>
+                <form onSubmit={handleFormSubmit}>
                     <div className='login-box__inputs-wrapper'>
                         <div
                             className='login-box__field-wrapper'
                         >
-                            <label
-                                className='login-box__input-label'
-                            >
-                                Email
-                            </label>
+                            <div>
+                                <label
+                                    className='login-box__input-label'
+                                >
+                                    Email
+                                </label>
+                                {
+                                    formInformation.email.error &&
+                                    <span className='login-box__input-error-text'>
+                                        {formInformation.email.error}
+                                    </span>
+                                }
+                            </div>
                             <input
                                 type='text'
-                                className='login-box__input'
+                                className={joinTruthy([
+                                    'login-box__input',
+                                    formInformation.email.error && 'error'
+                                ])}
                                 placeholder='your@email.here'
                                 name='email'
-                                required
                                 onBlur={handleEmailInputOnBlur}
                             />
                         </div>
                         <div
                             className='login-box__field-wrapper'
                         >
-                            <label
-                                className='login-box__input-label'
-                            >
-                                Password
-                            </label>
+                            <div>
+                                <label
+                                    className='login-box__input-label'
+                                >
+                                    Password
+                                </label>
+                                {
+                                    formInformation.password.error &&
+                                    <span className='login-box__input-error-text'>
+                                        {formInformation.password.error}
+                                    </span>
+                                }
+                            </div>
                             <input
                                 type='password'
-                                className='login-box__input'
+                                className={joinTruthy([
+                                    'login-box__input',
+                                    formInformation.password.error && 'error'
+                                ])}
                                 placeholder='********'
                                 name='psw'
-                                required
                                 onBlur={handlePasswordInputOnBlur}
                             />
                         </div>
@@ -91,7 +175,6 @@ export const Sidebar: React.FunctionComponent = () => {
                     </div>
                     <Button
                         type='submit'
-                        onClick={handleFormSubmit}
                         className='login-box__submit-button'
                     >
                         Login
